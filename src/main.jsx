5b4ae231c268ebca2b326d5312e901765e5ce2b5
egan <nzukiegan6@gmail.com>
2025-11-07 18:00:57 +0300

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 // src/main.jsx
+import { Buffer } from "buffer";
+window.Buffer = Buffer;
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { Buffer } from "buffer";
-window.Buffer = Buffer;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
