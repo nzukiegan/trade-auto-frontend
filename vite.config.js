782bdbd3f7f456331bfa85bbb54f8f027fbbe74e
egan <nzukiegan6@gmail.com>
2025-11-07 19:49:51 +0300

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
   resolve: {
     alias: {
       buffer: 'buffer',
+      process: 'process/browser',
     },
   },
   optimizeDeps: {
@@ -19,11 +17,17 @@ export default defineConfig({
         global: 'globalThis',
       },
       plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin()
+        NodeGlobalsPolyfillPlugin({ buffer: true, process: true }),
+        NodeModulesPolyfillPlugin(),
       ],
     },
   },
-})
+  build: {
+    rollupOptions: {
+      // ensure buffer & process are included in final bundle
+      plugins: [
+        NodeGlobalsPolyfillPlugin({ buffer: true, process: true }),
+      ],
+    },
+  },
+});
